0501e33c3485ac670356e616b67223bfa19cfdaa
Mustafa <mustafa.ramazanov.2002@gmail.com>
2023-05-31 12:24:03 +0300

    new commit

--- js/main.js
+++ js/main.js
@@ -16,12 +16,20 @@ function open(evt){
   tabTarget.classList.add('tabs__btn-item--active');
 
   tabContent.forEach(function(item){
-    item.classList.remove('tabs__content-item--active')
+    item.classList.remove('tabs__content-item--active');
   });
 
-  document.querySelector(`#${button}`).classList.add('tabs__btn-item--active');
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
 }
 
+
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu__list--active');
+})
 
 
 const swiper = new Swiper(".swiper", {
